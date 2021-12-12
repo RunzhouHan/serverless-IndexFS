20a38464328ce103de74138411ddc6338be3e528
RunzhouHan <rzhan@bu.edu>
2021-12-12 17:12:44 -0600

    reorganized serverless IndexFS folder and package names

---
+++ serverless/src/main/java/indexfs/serverless/Config.java
@@ -0,0 +1,217 @@
+package main.java.indexfs.serverless;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
+public class Config {
+	
+	/**
+	 * Serverless IndexFS server ID. Hardcoded to be 0 for now.
+	 */
+	private int serverless_server_id;  
+	
+	/**
+	 * IP of the server to handle the first request for MetaDB configuration
+	 */
+	private String zeroth_server;
+	
+	/**
+	 * The port of zeroth server. 
+	 */
+	private int zeroth_port;
+	
+	/**
+	 * The port of serverless TCP server. This is a hard coded port number. 
+	 */
+	private int tcp_port;
+	
+	/**
+	 * The port of serverless TCP server. This is a hard coded port number. 
+	 */
+	private int http_port;
+	
+	/**
+	 * Total number of MetaDB servers.
+	 */
+	private short NumofMetaDBs;
+
+	/**
+	 * List of MetaDB server IPs.
+	 */
+	private List<String> MetaDB_list;
+	
+	/**
+	 * List of MetaDB server port.
+	 */
+	private List<Integer> port_list;
+	
+	/**
+	 * MetaDB server ID to IP map.
+	 */
+	private Map<Integer, String> MetaDB_map;
+
+	/**
+	 * Serverless IndexFS LRU cache capacity.
+	 */
+	public final int LRU_capacity = 100000; // need to check IndexFS setting
+	
+	/**
+	 * Serverless IndexFS write-back cache capacity before commit.
+	 * Used when group a number of write operations to the same server.
+	 */
+	public final int NumtoCommit = 1000; // need to check IndexFS setting
+	
+	/**
+	 * Serverless IndexFS write-back cache commit time limit.
+	 */
+	public final int TimetoCommit = 1000; // need to check IndexFS setting
+
+	
+	/**
+	 * Constructor.
+	 * 
+	 * @param zeroth_server the first server for server information request.
+	 * @param zeroth_port the first server's port numnber.
+	 * @param serverless_server_id serverless IndexFS server ID.
+	 * @param tcp_port the port number of serverless TCP payload server.
+	 */
+	public Config(String zeroth_server, int zeroth_port, 
+			int serverless_server_id, int tcp_port) {
+		this.zeroth_server = zeroth_server;
+		this.zeroth_port = zeroth_port;
+		this.serverless_server_id = serverless_server_id;
+		this.MetaDB_list = BuildMetaDBList();
+		this.port_list = BuildPortList();
+		this.NumofMetaDBs = CountMetaDBNum();
+		this.MetaDB_map = new HashMap<Integer, String>();
+		this.MetaDB_map = BuildMetaDBMap();
+		this.tcp_port= tcp_port;
+	}
+	
+	/**
+	 *  Send RPC to MetaDB for MetaDB cluster IP list
+	 * @return list of MetaDB cluster IP address.
+	 */
+	private List<String> BuildMetaDBList() {
+		Serverless_IndexFS_ctx mdb_svrless_ctx = new Serverless_IndexFS_ctx();
+		TTransport socket = new TSocket(zeroth_server,zeroth_port);
+		TProtocol protocol = new TBinaryProtocol(socket);
+    	MetaDBService.Client mdb_client = new MetaDBService.Client(protocol);
+    	MetaDB_list = null;
+    	try {
+			socket.open();
+			MetaDB_list = mdb_svrless_ctx.GetServerList(mdb_client);
+	    	socket.close(); 
+		} catch (TTransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+    	return MetaDB_list;
+	}
+	
+	/**
+	 *  Send RPC to MetaDB for MetaDB cluster IP list
+	 * @return list of MetaDB cluster IP address.
+	 */
+	private List<Integer> BuildPortList() {
+		Serverless_IndexFS_ctx mdb_svrless_ctx = new Serverless_IndexFS_ctx();
+		TTransport socket = new TSocket(zeroth_server,zeroth_port);
+		TProtocol protocol = new TBinaryProtocol(socket);
+    	MetaDBService.Client mdb_client = new MetaDBService.Client(protocol);
+    	port_list = null;
+    	try {
+			socket.open();
+			port_list = mdb_svrless_ctx.GetPortList(mdb_client);
+	    	socket.close(); 
+		} catch (TTransportException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+    	return port_list;
+	}
+	
+	/**
+	 * Get total number of MetaDB servers.
+	 * @return number of MetaDB servers.
+	 */
+	private short CountMetaDBNum() {
+		// Number of server usually will not exceed short's capacity
+		return (short) MetaDB_list.size();
+	}
+	
+	/**
+	 * Build MetaDB server IP to ID map
+	 * @return MetaDB server IP to ID map.
+	 */
+	private Map<Integer, String> BuildMetaDBMap() {
+		int sid = 0;
+		for (String srv_ip : MetaDB_list ) {
+			MetaDB_map.put(sid, srv_ip);
+			sid++;
+		}
+		return MetaDB_map;
+	}
+	
+	/**
+	 * A public API to get serverless TCP server port.
+	 * @return serverless TCP server port.
+	 */
+	public int GetTCPPort() {
+		return this.tcp_port;
+	}
+	
+	/**
+	 * A public API to get serverless HTTP server port.
+	 * @return serverless TCP server port.
+	 */
+	public int GetHTTPPort() {
+		return this.http_port;
+	}
+	
+	/**
+	 * A public API to get serverless IndexFS server ID.
+	 * @return serverless IndexFS server ID.
+	 */
+	public int GetSrvID() {
+		return this.serverless_server_id;
+	}
+	
+	/**
+	 * A public API to get total number of MetaDB servers.
+	 * @return Number of MetaDB in total.
+	 */
+	public short GetMetaDBNum() {
+		return this.NumofMetaDBs;
+	}
+	
+	/**
+	 * A public API to get serverless IndexFS server ID.
+	 * @return MetaDB server IP list.
+	 */
+	public List<String> GetMetaDBList() {
+		return this.MetaDB_list;
+	}
+	
+	/**
+	 * A public API to get serverless IndexFS server ID.
+	 * @return MetaDB server port list.
+	 */
+	public List<Integer> GetPortList() {
+		return this.port_list;
+	}
+	
+	/**
+	 * A public API to get serverless IndexFS server ID.
+	 * @return MetaDB server ID-IP mapping.
+	 */
+	public Map<Integer, String> GetMetaDBMap() {
+		return this.MetaDB_map;
+	}
+}
---
+++ serverless/src/main/java/indexfs/serverless/Serverless_IndexFS_main.java
@@ -0,0 +1,147 @@
+package main.java.indexfs.serverless;
+
+import java.io.IOException;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+//import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Serverless_IndexFS_main {
+	
+	public static final String PACKAGE_VERSION = "0.0.0";
+	
+	public static boolean SERVER_START = false;
+		
+	public static final String FLAGS_logfn = "indexfs_server";
+	
+	public static final int FLAGS_srvid = -1;
+	  
+	public static final int FLAGS_logbufsecs = 5;
+	
+	public static final String FLAGS_log_dir = "/";
+	
+	public static Serverless_IndexFS_driver driver;
+	
+	public static final Logger LOG = LoggerFactory.getLogger(Serverless_IndexFS_main.class.getName());
+	
+	public static String zeroth_server;
+	
+	public static String zeroth_port;
+	
+	public static String instance_id;
+	
+	public static String deployment_id;
+	
+	public static String op_type;
+	
+	public static String path;
+	
+	public static JsonObject OID;
+//	
+//	public static void SignalHandler(int sig) {
+//	  if (driver != null) {
+//	    driver.Shutdown();
+//	  }
+//	  LOG.info("Receive external signal to stop server " + deployment_id + " ...");
+//	}
+	
+	/**
+	 * serverless version of main method.
+	 * @param args
+	 * @return
+	 */
+	public static JsonObject main(JsonObject args) { //serverless run uncomment this.
+		/*
+		// Terminate the serverless IndexFS server when receive SIGTERM sent from Kubernetes.
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+        @Override
+            public void run() {
+                System.out.println("Shutdown Hook Registered.");
+            }   
+        }); 
+		
+        try {
+		// Management server IP address.
+		zeroth_server = args.getAsJsonPrimitive(srvless_IndexFS_keys.zeroth_server).getAsString();  //serverless run uncomment this
+        } catch (JsonParseException ex) {
+        	LOG.info("Please provide IP address of the management server.");
+        }
+        
+        try {
+		// Management server port.
+		zeroth_port = args.getAsJsonPrimitive(srvless_IndexFS_keys.zeroth_port).getAsString();  //serverless run uncomment this
+        } catch (JsonParseException ex) {
+        	LOG.info("Please provide port number of the management server.");
+        }
+        
+        try {        
+		// Pod ID.
+		instance_id = args.getAsJsonPrimitive(srvless_IndexFS_keys.instance_id).getAsString();  //serverless run uncomment this	
+	    } catch (JsonParseException ex) {
+	    	LOG.info("Please provide instance id.");
+	    }
+        
+        try {
+		// Serverless function ID (IndexFS server ID). 
+		deployment_id = args.getAsJsonPrimitive(srvless_IndexFS_keys.deployment_id).getAsString();  //serverless run uncomment this
+		} catch (JsonParseException ex) {
+			LOG.info("Please provide deployment id for the serverless function.");
+		}
+        
+        try {
+		// Operation type (a metadata operation parameter).
+		path = args.getAsJsonPrimitive(srvless_IndexFS_keys.op_type).getAsString();  //serverless run uncomment this
+	    } catch (JsonParseException ex) {
+	    	LOG.info("Please provide the metadata operation type.");
+	    }
+        
+        try {
+		// Object path (a metadata operation parameter).
+		path = args.getAsJsonPrimitive(srvless_IndexFS_keys.path).getAsString();  //serverless run uncomment this
+	    } catch (JsonParseException ex) {
+	    	LOG.info("Please provide the object path of the metadata operation.");
+	    }
+	    
+        try {
+		// OID (a metadata operation parameter). 
+		OID = args.getAsJsonPrimitive(srvless_IndexFS_keys.OID).getAsJsonObject();
+	    } catch (JsonParseException ex) {
+	    	LOG.info("Please provide OID of the metadata operation." );
+	    }
+				
+
+		if (SERVER_START == false) {
+			LOG.info("============================================================");
+			LOG.info("set indexfs server rank " + deployment_id);
+			driver = new srvless_IndexFS_driver(zeroth_server, zeroth_port, instance_id, deployment_id); // serverless run uncomment this
+			try {
+				driver.StartServer();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			SERVER_START = true;
+		}
+		
+		if(SERVER_START) {
+			driver.proceedClientRequest(op_type, path, OID);
+		}
+		
+		LOG.info("Everything disposed, server will now shutdown");
+		*/
+		
+		JsonObject ret = new JsonObject();
+		ret.addProperty("return value", "Succeed!");
+		return ret;
+	}
+      
+	/**
+	 * Local main method.
+	 */
+//    public static void main(String[] args) throws IOException { // local run uncomment this
+//		  driver = new srvless_IndexFS_driver(args[0], args[1], args[2], args[3]); // local run uncomment this
+//	}
+}
