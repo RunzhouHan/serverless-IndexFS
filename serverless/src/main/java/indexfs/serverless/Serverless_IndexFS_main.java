404565a17a668bae23800f8545c1e7c524bfa0f9
RunzhouHan <rzhan@bu.edu>
2021-12-15 16:46:30 -0600

    debug main function

--- serverless/src/main/java/indexfs/serverless/Serverless_IndexFS_main.java
+++ serverless/src/main/java/indexfs/serverless/Serverless_IndexFS_main.java
@@ -53,8 +53,10 @@ public class Serverless_IndexFS_main {
 	 * @param args
 	 * @return
 	 */
+//  public static void main(String[] args_) throws IOException { // local run uncomment this
+
 	public static JsonObject main(JsonObject args) { //serverless run uncomment this.
-		/*
+		
 		// Terminate the serverless IndexFS server when receive SIGTERM sent from Kubernetes.
         Runtime.getRuntime().addShutdownHook(new Thread() {
         @Override
@@ -62,53 +64,60 @@ public class Serverless_IndexFS_main {
                 System.out.println("Shutdown Hook Registered.");
             }   
         }); 
+        
 		
-        try {
+//		JsonObject args = new JsonObject();
+//		args.addProperty("zeroth_server", "127.0.0.1");
+//		args.addProperty("zeroth_port", "10086");
+//		args.addProperty("instance_id", "0");
+//		args.addProperty("deployment_id", "0");
+		
+		if (args.has(Serverless_IndexFS_keys.zeroth_server)) {
 		// Management server IP address.
-		zeroth_server = args.getAsJsonPrimitive(srvless_IndexFS_keys.zeroth_server).getAsString();  //serverless run uncomment this
-        } catch (JsonParseException ex) {
+        	zeroth_server = args.getAsJsonPrimitive(Serverless_IndexFS_keys.zeroth_server).getAsString();  //serverless run uncomment this
+        } else {
         	LOG.info("Please provide IP address of the management server.");
         }
         
-        try {
+		if (args.has(Serverless_IndexFS_keys.zeroth_port))  {
 		// Management server port.
-		zeroth_port = args.getAsJsonPrimitive(srvless_IndexFS_keys.zeroth_port).getAsString();  //serverless run uncomment this
-        } catch (JsonParseException ex) {
+        	zeroth_port = args.getAsJsonPrimitive(Serverless_IndexFS_keys.zeroth_port).getAsString();  //serverless run uncomment this
+        } else {
         	LOG.info("Please provide port number of the management server.");
         }
         
-        try {        
+        if (args.has(Serverless_IndexFS_keys.instance_id))  {        
 		// Pod ID.
-		instance_id = args.getAsJsonPrimitive(srvless_IndexFS_keys.instance_id).getAsString();  //serverless run uncomment this	
-	    } catch (JsonParseException ex) {
+        	instance_id = args.getAsJsonPrimitive(Serverless_IndexFS_keys.instance_id).getAsString();  //serverless run uncomment this	
+	    } else {
 	    	LOG.info("Please provide instance id.");
 	    }
         
-        try {
+	    if (args.has(Serverless_IndexFS_keys.deployment_id))  {
 		// Serverless function ID (IndexFS server ID). 
-		deployment_id = args.getAsJsonPrimitive(srvless_IndexFS_keys.deployment_id).getAsString();  //serverless run uncomment this
-		} catch (JsonParseException ex) {
+        	deployment_id = args.getAsJsonPrimitive(Serverless_IndexFS_keys.deployment_id).getAsString();  //serverless run uncomment this
+		} else {
 			LOG.info("Please provide deployment id for the serverless function.");
 		}
         
-        try {
+		if (args.has(Serverless_IndexFS_keys.op_type))  {
 		// Operation type (a metadata operation parameter).
-		path = args.getAsJsonPrimitive(srvless_IndexFS_keys.op_type).getAsString();  //serverless run uncomment this
-	    } catch (JsonParseException ex) {
+        	path = args.getAsJsonPrimitive(Serverless_IndexFS_keys.op_type).getAsString();  //serverless run uncomment this
+	    } else {
 	    	LOG.info("Please provide the metadata operation type.");
 	    }
         
-        try {
+	    if (args.has(Serverless_IndexFS_keys.path))  {
 		// Object path (a metadata operation parameter).
-		path = args.getAsJsonPrimitive(srvless_IndexFS_keys.path).getAsString();  //serverless run uncomment this
-	    } catch (JsonParseException ex) {
+        	path = args.getAsJsonPrimitive(Serverless_IndexFS_keys.path).getAsString();  //serverless run uncomment this
+	    } else {
 	    	LOG.info("Please provide the object path of the metadata operation.");
 	    }
 	    
-        try {
+	    if (args.has(Serverless_IndexFS_keys.OID))  {
 		// OID (a metadata operation parameter). 
-		OID = args.getAsJsonPrimitive(srvless_IndexFS_keys.OID).getAsJsonObject();
-	    } catch (JsonParseException ex) {
+        	OID = args.getAsJsonPrimitive(Serverless_IndexFS_keys.OID).getAsJsonObject();
+	    } else {
 	    	LOG.info("Please provide OID of the metadata operation." );
 	    }
 				
@@ -117,12 +126,12 @@ public class Serverless_IndexFS_main {
 			LOG.info("============================================================");
 			LOG.info("set indexfs server rank " + deployment_id);
 			driver = new Serverless_IndexFS_driver(zeroth_server, zeroth_port, instance_id, deployment_id); // serverless run uncomment this
-			try {
-				driver.StartServer();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+//			try {
+//				driver.StartServer();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
 			SERVER_START = true;
 		}
 		
@@ -131,11 +140,11 @@ public class Serverless_IndexFS_main {
 		}
 		
 		LOG.info("Everything disposed, server will now shutdown");
-		*/
 		
-		JsonObject ret = new JsonObject();
-		ret.addProperty("return value", "Succeed!");
-		return ret;
+		
+//		JsonObject ret = new JsonObject();
+//		ret.addProperty("return value", "Succeed!");
+		return args;
 	}
       
 	/**
