efc8587261d1d47a54b363b67142b7fd0ae945cf
RunzhouHan <rzhan@bu.edu>
2022-09-28 02:08:36 -0500

    test while true

--- client/tcp_socket.cc
+++ client/tcp_socket.cc
@@ -140,87 +140,90 @@ Status tcp_socket::connect(const char* msg) {
     }
 
 
-    //If something happened on the master socket , 
-    //then its an incoming connection 
-    if (FD_ISSET(server_sockfd, &readfds))  
-    {  
-        if ((conn = accept(server_sockfd, 
-                (struct sockaddr *)&client_addr, (socklen_t*)&length))<0)  
+    while (true) {
+
+        //If something happened on the master socket , 
+        //then its an incoming connection 
+        if (FD_ISSET(server_sockfd, &readfds))  
         {  
-            perror("accept");  
-            exit(EXIT_FAILURE);  
-        }  
-         
-        //inform user of socket number - used in send and receive commands 
-        printf("New connection, socket fd is %d, ip is : %s, port: %d\n", 
-            conn , inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));  
-       
-        //send new connection greeting message 
-        // if( send(new_socket, message, strlen(message), 0) != strlen(message) )  
-        // {  
-        //     perror("send");  
-        // }  
-        // puts("Welcome message sent successfully");  
+            if ((conn = accept(server_sockfd, 
+                    (struct sockaddr *)&client_addr, (socklen_t*)&length))<0)  
+            {  
+                perror("accept");  
+                exit(EXIT_FAILURE);  
+            }  
              
-        //add new socket to array of sockets 
+            //inform user of socket number - used in send and receive commands 
+            printf("New connection, socket fd is %d, ip is : %s, port: %d\n", 
+                conn , inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));  
+           
+            //send new connection greeting message 
+            // if( send(new_socket, message, strlen(message), 0) != strlen(message) )  
+            // {  
+            //     perror("send");  
+            // }  
+            // puts("Welcome message sent successfully");  
+                 
+            //add new socket to array of sockets 
+            for (int i = 0; i < MAX_CLIENTS; i++)  
+            {  
+                //if position is empty 
+                if(client_socket[i] == 0 )  
+                {  
+                    client_socket[i] = conn;  
+                    printf("Client accepted at rank %d, Adding to list of sockets as %d\n",my_rank_, i);  
+                    break;  
+                }  
+            }  
+        }
+
+        char recv_buf[65536];
+        memset(recv_buf, '\0', sizeof(recv_buf));
+        char client_ip[INET_ADDRSTRLEN] = "";
+
+        //else its some IO operation on some other socket
         for (int i = 0; i < MAX_CLIENTS; i++)  
         {  
-            //if position is empty 
-            if(client_socket[i] == 0 )  
-            {  
-                client_socket[i] = conn;  
-                printf("Client accepted at rank %d, Adding to list of sockets as %d\n",my_rank_, i);  
-                break;  
-            }  
-        }  
-    }
-
-    char recv_buf[65536];
-    memset(recv_buf, '\0', sizeof(recv_buf));
-    char client_ip[INET_ADDRSTRLEN] = "";
-
-    //else its some IO operation on some other socket
-    for (int i = 0; i < MAX_CLIENTS; i++)  
-    {  
-        sd = client_socket[i];  
-             
-        if (FD_ISSET(sd , &readfds))  
-        {   
-            /*
-            //Check if it was for closing , and also read the 
-            //incoming message 
-            if ((valread = read( sd , recv_buf, 1024)) == 0)  
-            {  
-                //Somebody disconnected , get his details and print 
-                getpeername(sd , (struct sockaddr*)&client_addr , \
-                    (socklen_t*)&length);  
-                printf("Host disconnected , ip %s , port %d \n" , 
-                      inet_ntoa(client_addr.sin_addr) , ntohs(client_addr.sin_port));  
-                     
-                //Close the socket and mark as 0 in list for reuse 
-                close( sd );  
-                client_socket[i] = 0;  
-            }  
+            sd = client_socket[i];  
                  
-            //Echo back the message that came in 
-            else 
-            {  
-                //set the string terminating NULL byte on the end 
-                //of the data read 
-                buffer[valread] = '\0';  
-                send(sd , buffer , strlen(buffer) , 0 );  
+            if (FD_ISSET(sd , &readfds))  
+            {   
+                /*
+                //Check if it was for closing , and also read the 
+                //incoming message 
+                if ((valread = read( sd , recv_buf, 1024)) == 0)  
+                {  
+                    //Somebody disconnected , get his details and print 
+                    getpeername(sd , (struct sockaddr*)&client_addr , \
+                        (socklen_t*)&length);  
+                    printf("Host disconnected , ip %s , port %d \n" , 
+                          inet_ntoa(client_addr.sin_addr) , ntohs(client_addr.sin_port));  
+                         
+                    //Close the socket and mark as 0 in list for reuse 
+                    close( sd );  
+                    client_socket[i] = 0;  
+                }  
+                     
+                //Echo back the message that came in 
+                else 
+                {  
+                    //set the string terminating NULL byte on the end 
+                    //of the data read 
+                    buffer[valread] = '\0';  
+                    send(sd , buffer , strlen(buffer) , 0 );  
+                }  
+                */
+                inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+
+                while(recv(sd, recv_buf, sizeof(recv_buf), 0) > 0 ){
+                    printf("recv: %s from serverless IndexFS (%s:%d). \n", recv_buf, client_ip, ntohs(client_addr.sin_port));
+                    memset(recv_buf, '\0', strlen(recv_buf));
+                    break;
+                }
+
             }  
-            */
-            inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
-
-            while(recv(sd, recv_buf, sizeof(recv_buf), 0) > 0 ){
-                printf("recv: %s from serverless IndexFS (%s:%d). \n", recv_buf, client_ip, ntohs(client_addr.sin_port));
-                memset(recv_buf, '\0', strlen(recv_buf));
-                break;
-            }
-
-        }  
-    } 
+        } 
+    }
 
 
 
